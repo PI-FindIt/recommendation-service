91506e8987d47f4562b3ce5c9383360a7e582308
Rúben Garrido <57329376+RGarrido03@users.noreply.github.com>
2025-04-22 18:21:36 +0100

    foda-se

--- src/config.py
+++ src/config.py
@@ -13,7 +13,7 @@ class Settings(BaseSettings):
     REDIS_HOST: str = os.getenv("REDIS_HOST", "localhost")
     REDIS_PORT: int = os.getenv("REDIS_PORT", 6379)
     REDIS_DB: int = os.getenv("REDIS_DB", 0)
-    REDIS_URL = f"redis://{REDIS_HOST}:{REDIS_PORT}/{REDIS_DB}"
+    REDIS_URL: str = f"redis://{REDIS_HOST}:{REDIS_PORT}/{REDIS_DB}"
 
 
 settings = Settings()
