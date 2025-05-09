b9c8941138e2f905eff1fc546d625d6ba3d917d6
andre <andrepedro2004@hotmail.com>
2025-05-09 22:49:38 +0100

    ahh dor

--- src/models/user_recommendation_engine.py
+++ src/models/user_recommendation_engine.py
@@ -434,7 +434,7 @@ class UserRecommendationEngine(BaseEngine):
         for candidate in candidates:
             score = candidate["score"]
             if a := candidate.get("product", {}).get("supermarkets"):
-                print("FARTO ", a)
+                print("FARTO ", a, "candidate ", candidate.get("product", {}))
                 supermarket_ids = [
                     s["supermarket"]["id"] for s in candidate["product"]["supermarkets"]
                 ]
